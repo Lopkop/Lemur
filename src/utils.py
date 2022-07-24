958795d2c9b9501d08b80fa657573b8f73d7b46f
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-07-24 17:12:29 +0300

    Add documentation to utils.py function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import secrets
 
 
 def generate_random_id() -> str:
+    """Generates a secure random identifier"""
     with open('wordlists/nouns.txt') as nouns, open('wordlists/adjectives.txt') as adjectives:
         nouns = [noun.strip() for noun in nouns]
         adjectives = [adj.strip() for adj in adjectives]
