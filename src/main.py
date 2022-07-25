5ee85e61bc5ae19084cb8f395cf204fcba41ff6a
Bartosz Raubo <braubo@8thlight.com>
2022-07-25 21:35:12 +0100

    add send message and get message routes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi import WebSocket
 
 app = FastAPI()
+# Instantiate DatabaseClient
 
 @app.websocket("/sign_up")
 async def sign_up(websocket: WebSocket):
@@ -12,4 +13,23 @@ async def sign_up(websocket: WebSocket):
     # Create User Object
     # Save User To DB
     response = {} # uses ResponseFactory to generate Response JSON from User Object
+    connection.send_response(response)
+
+@app.websocket("/send_message")
+async def send_message(websocket: WebSocket):
+    ''' Sends Message to Given Chatroom '''
+    connection = ConnectionHandler(websocket)
+    request = connection.get_request()
+    message = {} # Create Message Object
+    # Save Message Object to DB
+    response = {} # uses ResponseFactory to generate Response JSON from Message Object
+    connection.send_response(response)
+
+@app.websocket("/get_messages")
+async def send_message(websocket: WebSocket):
+    ''' Sends Message to Given Chatroom '''
+    connection = ConnectionHandler(websocket)
+    request = connection.get_request()
+    chatroom = {} # Get messsage object from chatroom - something like database_client.get_chatroom_data(chatroom_id)
+    response = {} # uses ResponseFactory to generate Response JSON from Chatroom Object
     connection.send_response(response)
