e6978bb8761d9c37e8b39242993f1af334a108a7
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-07-25 11:20:46 +0300

    Add documentation to RandomIdGenerator class

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import secrets
 
 
 class RandomIdGenerator:
+    """By calling generates random identifier"""
     def __init__(self):
         """Opens wordlists and store in memory"""
         with open('wordlists/nouns.txt') as nouns, open('wordlists/adjectives.txt') as adjectives:
