921a6c4d57a4440999f926dc1d4e75fad631e84e
Bartosz Raubo <braubo@8thlight.com>
2022-07-25 19:58:31 +0100

    fixed docstring

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.websocket("/sign_up")
 async def sign_up(websocket: WebSocket):
-    """ Signs Up New Users """
+    ''' Signs Up New Users '''
     connection = ConnectionHandler(websocket)
     request = connection.get_request()
     # Create User Object
