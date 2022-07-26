65aaa6f4a6335336ff9105dd4c87834bff44371e
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-07-26 22:06:34 +0300

    Add MessageModel, ChatRoomModel, SingUpResponseModel and SendMessageResponseModel

--- src/models.py
+++ src/models.py
@@ -1,9 +1,36 @@
 from pydantic import BaseModel
 
 
-class User(BaseModel):
+class UserModel(BaseModel):
     """User object model"""
 
+    name: str
     user_id: int
     name: str
     chatroom_id: str
+
+
+class MessageModel(BaseModel):
+    """Message object model"""
+
+    message_id: int
+    body: str
+    timestamp: int  # todo: should be replaced (ex: datetime.datetime)
+
+
+class ChatRoomModel(BaseModel):
+    """ChatRoom object model"""
+
+    chatroom_id: int
+    messages: list[MessageModel]
+    user_id: int
+
+
+class SingUpResponseModel(BaseModel):
+    status: bool
+    user: UserModel
+
+
+class SendMessageResponseModel(BaseModel):
+    status: bool
+    message: MessageModel
