317a53a4ceb872721d6d47b5b31c5233ee13b9c8
Bartosz Raubo <braubo@8thlight.com>
2022-07-26 23:14:25 +0100

    Sign up response test

---
+++ src/tests/test_response_factory.py
@@ -0,0 +1,22 @@
+from src.models import UserModel
+from src.sockets.response_factory import ResponseFactory
+
+test_data = {
+    "new_user": UserModel(name="funky_goblin", user_id="a-user-id", chatroom_id="a-chatroom-id")
+}
+
+def test_sign_up_response():
+    user = test_data["new_user"]
+    response = ResponseFactory.generate_sign_up_response(status=True, user_model=user)
+    
+    expected_response = {
+        'status': True,
+        'user': {
+            'chatroom_id': 'a-chatroom-id',
+            'name': 'funky_goblin',
+            'user_id': 'a-user-id'
+        }
+    }
+
+    assert response == expected_response
+   
