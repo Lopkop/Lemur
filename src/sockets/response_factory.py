fe786a9e15f06548042cede2d6283b39bd5a13ed
Bartosz Raubo <braubo@8thlight.com>
2022-07-26 23:13:56 +0100

    oops

--- src/sockets/response_factory.py
+++ src/sockets/response_factory.py
@@ -4,17 +4,17 @@ from src.models import UserModel, MessageModel, ChatRoomModel, ChatRoomResponseM
 
 class ResponseFactory:
 
-    def generate_sign_up_response(status: bool, user_model: UserModel) -> str:
+    def generate_sign_up_response(status: bool, user_model: UserModel) -> dict:
         """Generates sign up response in JSON"""
         response_model = SignUpResponseModel(status=status, user=user_model)
         return response_model.dict()
 
-    def generate_send_message_response(status: bool, message: MessageModel) -> str:
+    def generate_send_message_response(status: bool, message: MessageModel) -> dict:
         """Generates send_message response in JSON"""
         response_model = SendMessageResponseModel(status=status, message=message)
         return response_model.dict()
 
-    def generate_get_messages_response(chatroom: ChatRoomModel) -> str:
+    def generate_get_messages_response(chatroom: ChatRoomModel) -> dict:
         """Generates get_messages response in JSON"""
         chatroom_model = ChatRoomResponseModel(chatroom)
         return chatroom_model.dict()
