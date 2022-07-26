80dace185697ff26be9963827ed4d5c5ebe78552
Bartosz Raubo <braubo@8thlight.com>
2022-07-26 22:39:21 +0100

    fix typo

--- src/sockets/response_factory.py
+++ src/sockets/response_factory.py
@@ -1,11 +1,11 @@
 from src.db.models.messages_model import ChatRoom
-from src.models import UserModel, MessageModel, ChatRoomModel, ChatRoomResponseModel, SingUpResponseModel, SendMessageResponseModel
+from src.models import UserModel, MessageModel, ChatRoomModel, ChatRoomResponseModel, SignUpResponseModel, SendMessageResponseModel
 
 
 class ResponseFactory:
     def generate_sign_up_response(self, status: bool, user_model: UserModel) -> str:
         """Generates sign up response in JSON"""
-        response_model = SingUpResponseModel(status=status, user=user_model)
+        response_model = SignUpResponseModel(status=status, user=user_model)
         return response_model.json()
 
     def generate_send_message_response(self, status: bool, message: MessageModel) -> str:
