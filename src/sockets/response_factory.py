f22f1cd2bae7cb787b377203097244267c374f55
Bartosz Raubo <braubo@8thlight.com>
2022-07-26 22:45:27 +0100

    removed self keyword as we do not init

--- src/sockets/response_factory.py
+++ src/sockets/response_factory.py
@@ -3,17 +3,17 @@ from src.models import UserModel, MessageModel, ChatRoomModel, ChatRoomResponseM
 
 
 class ResponseFactory:
-    def generate_sign_up_response(self, status: bool, user_model: UserModel) -> str:
+    def generate_sign_up_response(status: bool, user_model: UserModel) -> str:
         """Generates sign up response in JSON"""
         response_model = SignUpResponseModel(status=status, user=user_model)
         return response_model.json()
 
-    def generate_send_message_response(self, status: bool, message: MessageModel) -> str:
+    def generate_send_message_response(status: bool, message: MessageModel) -> str:
         """Generates send_message response in JSON"""
-        response_model = SendMessageResponseModel(status=status, message=message_model)
+        response_model = SendMessageResponseModel(status=status, message=message)
         return response_model.json()
 
-    def generate_get_messages_response(self, chatroom: ChatRoomModel) -> str:
+    def generate_get_messages_response(chatroom: ChatRoomModel) -> str:
         """Generates get_messages response in JSON"""
         chatroom_model = ChatRoomResponseModel(chatroom)
         return chatroom_model.json()
