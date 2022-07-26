83c355f46cc9068ec3bfede85669b3d70a9f9be9
Bartosz Raubo <braubo@8thlight.com>
2022-07-26 22:37:26 +0100

    fix connection handler - we dont want to pass around strings

--- src/sockets/connection_handler.py
+++ src/sockets/connection_handler.py
@@ -1,8 +1,10 @@
+import json
+
 class ConnectionHandler:
     def __init__(self, socket):
         self.socket = socket
 
-    async def get_request(self) -> str:
+    async def get_request(self) -> dict:
         """Returns json request from client"""
         return await self.process_incoming_connection()
 
@@ -10,8 +12,11 @@ class ConnectionHandler:
         """Sends json response to the client"""
         await self.socket.send_text(response)
 
-    async def process_incoming_connection(self) -> str:
+    async def process_incoming_connection(self) -> dict:
         """Accepts connection and returns request"""
         await self.socket.accept()
         request = await self.socket.receive_text()
-        return request
+        return self.parse_request(request)
+    
+    def parse_request(self, request: str) -> dict:
+        return json.loads(request)
