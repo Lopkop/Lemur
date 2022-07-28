84ffd4372b4d866ed3ca4382f7b28e90900c0899
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-07-28 13:12:54 +0300

    Add changes to DB API

--- src/db/db.py
+++ src/db/db.py
@@ -1,3 +1,4 @@
+from src.db.models.chat_room_model import ChatRoom
 from src.db.models.messages_model import Message
 from src.db.models.user_model import User
 from src.db.session import Base, Session, engine
@@ -14,62 +15,46 @@ class DatabaseService:
         """Saves user object to database"""
         user_db_model = User(
             name=user_obj.name,
-            chat_room_id=user_obj.chatroom_id,
             user_id=user_obj.user_id,
         )
         Session.add(user_db_model)
         Session.commit()
 
     def fetch_user_by_id(self, user_id: str) -> UserModel:
-        """Get User by id
-
-        Fetch user object by querying on name
-        :param name:
-        :return:
-        """
-        user_db_obj = Session.query(User).filter_by(user_id=id).first()
+        """Fetch User by user_id"""
+        user_db_obj = Session.query(User).filter_by(user_id=user_id).first()
         return create_and_get_user(
             user_id=user_db_obj.user_id,
-            chatroom_id=user_db_obj.chat_room_id,
             username=user_db_obj.name,
         )
 
-    def save_messaage(self, chatroom_obj: ChatRoomModel, message_obj: MessageModel):
-        """
-        Saves a message to chatroom
+    def save_chatroom(self, chatroom_obj: ChatRoomModel) -> None:
+        chatroom_db_obj = ChatRoom(chat_room_id=chatroom_obj.chatroom_id, user_one=chatroom_obj.users[0])
+        Session.add(chatroom_db_obj)
+        Session.commit()
 
-        :param chatroom_obj:
-        :param message_obj:
-        :return:
-        """
-        user_item = Session.query(User).filter_by(user_id=message_obj.user_id).first()
+    def save_message(self, chatroom_id: str, message_obj: MessageModel) -> None:
+        """Saves a message to the chatroom"""
         message_obj = Message(
-            chat_room_id=chatroom_obj.chatroom_id,
-            user_id=user_item.id,
+            chat_room_id=chatroom_id,
+            user_id=message_obj.user_id,
             body=message_obj.body,
         )
         Session.add(message_obj)
         Session.commit()
 
     def get_chat_room_messages(
-        self, chatroom_obj: ChatRoomModel, page: int = 1, size: int = 20
+            self, chatroom_id: str, page: int = 1, size: int = 20
     ):
-        """
-            Fetch all messages in a chat room
-
-        :param chatroom_id:
-        :param page:
-        :param size:
-        :return:
-        """
+        """Fetch all messages in a chat room"""
         page = page - 1
         offset = page * size
-        chatroom_obj.messages = (
+        messages = (
             Session.query(Message)
-            .filter_by(chat_room_id=chatroom_obj.chatroom_id)
-            .order_by(Message.created_at.desc())
-            .limit(size)
-            .offset(offset)
-            .all()
+                .filter_by(chat_room_id=chatroom_id)
+                .order_by(Message.created_at.desc())
+                .limit(size)
+                .offset(offset)
+                .all()
         )
-        return chatroom_obj
+        return messages
