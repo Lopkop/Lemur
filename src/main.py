7bb07a45c2e71d4153f4868783557060fcdf6ada
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-08-15 13:28:38 +0300

    Refactor response_factory.py, add 2 endpoints in main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,45 @@
-import asyncio
+from typing import Union
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Depends
+from sqlalchemy.orm import scoped_session
 
 from sockets.connection_handler import ConnectionHandler
+from src.db.dbapi import DatabaseService
+from src.db import schemas
+from src.sockets.response_factory import ResponseFactory
+from src.utils import RandomIdGenerator, create_and_get_chatroom
 
 app = FastAPI()
+db = DatabaseService()
+response = ResponseFactory()
 
 
-# Instantiate DatabaseClient
-
-
-@app.websocket("/sign_up")
-async def sign_up(websocket: WebSocket):
+@app.post("/sign-up", response_model=schemas.SignUpResponseModel)
+async def sign_up(user: schemas.UserModel, session: scoped_session = Depends(db.get_db)):
     """Signs Up New Users"""
-    connection = ConnectionHandler(websocket)
-    request = connection.get_request()
-    # Create User Object
-    # Save User To DB
-    response = {}  # uses ResponseFactory to generate Response JSON from User Object
-    connection.send_response(response)
+    db.save_user(session, user)
+    if db.fetch_user_by_name(session, user.name):
+        return response.generate_sign_up_response(True, user)
+    return response.generate_sign_up_response(False, user)
 
 
+@app.post("/create-chat", response_model=Union[schemas.ChatRoomModel, dict])
+async def create_chat(user: schemas.UserModel, session: scoped_session = Depends(db.get_db)):
+    """Create chatroom and save to db"""
+    if not db.fetch_user_by_name(session, user.name):
+        return response.generate_user_undefined_error_response(user)
+
+    generate_id = RandomIdGenerator()
+    chatroom_name = generate_id()
+    chat = create_and_get_chatroom(user, name=chatroom_name)
+    db.save_chatroom(session, chat)
+
+    if db.fetch_chat_by_name(session, chatroom_name):
+        return response.generate_chat_response(True, chat)
+    return response.generate_chat_response(False, chat)
+
+
+# TODO: finish other api's
 @app.websocket("/send_message")
 async def send_message(websocket: WebSocket):
     """Sends Message to Given Chatroom"""
--- src/sockets/response_factory.py
+++ src/sockets/response_factory.py
@@ -1,25 +1,37 @@
 from src.db.schemas import (
     ChatRoomModel,
-    ChatRoomResponseModel,
+    GetMessagesResponseModel,
     MessageModel,
     SendMessageResponseModel,
     SignUpResponseModel,
-    UserModel,
+    UserModel, CreateChatResponseModel,
 )
 
 
 class ResponseFactory:
-    def generate_sign_up_response(status: bool, user_model: UserModel) -> dict:
+    @staticmethod
+    def generate_sign_up_response(status: bool, user_model: UserModel) -> SignUpResponseModel:
         """Generates sign up response in JSON"""
         response_model = SignUpResponseModel(status=status, user=user_model)
-        return response_model.dict()
+        return response_model
 
-    def generate_send_message_response(status: bool, message: MessageModel) -> dict:
+    @staticmethod
+    def generate_send_message_response(status: bool, message: MessageModel) -> SendMessageResponseModel:
         """Generates send_message response in JSON"""
         response_model = SendMessageResponseModel(status=status, message=message)
-        return response_model.dict()
+        return response_model
 
-    def generate_get_messages_response(chatroom: ChatRoomModel) -> dict:
+    @staticmethod
+    def generate_get_messages_response(status: bool, chatroom: ChatRoomModel) -> GetMessagesResponseModel:
         """Generates get_messages response in JSON"""
-        response_model = ChatRoomResponseModel(**chatroom.dict(exclude={"users"}))
-        return response_model.dict()
+        response_model = GetMessagesResponseModel(**chatroom.dict(exclude={"users"}), status=status)
+        return response_model
+
+    @staticmethod
+    def generate_chat_response(status: bool, chatroom: ChatRoomModel) -> CreateChatResponseModel:
+        response_model = CreateChatResponseModel(**chatroom.dict(), status=status)
+        return response_model
+
+    @staticmethod
+    def generate_user_undefined_error_response(user: UserModel) -> dict:
+        return {'status': 'error', 'user': user}
