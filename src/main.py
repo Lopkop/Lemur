a177d998de1d78aced85e2a5d89072b0d1fc8f71
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-09-23 13:11:51 +0300

    Refactor main.py, add apis (including one websocket api)
    There is a lot of work to do yet

--- src/main.py
+++ src/main.py
@@ -1,22 +1,39 @@
+from datetime import datetime
 from typing import Union
 
-from fastapi import FastAPI, WebSocket, Depends
+from fastapi import FastAPI, WebSocket, Depends, WebSocketDisconnect
 from sqlalchemy.orm import scoped_session
 
-from sockets.connection_handler import ConnectionHandler
+from sockets.connection_manager import ConnectionManager
 from src.db.dbapi import DatabaseService
 from src.db import schemas
+from src.db.schemas import MessageModel
 from src.sockets.response_factory import ResponseFactory
-from src.utils import RandomIdGenerator, create_and_get_chatroom
+from src.utils import RandomIdGenerator, create_and_get_chatroom, create_and_get_message
 
 app = FastAPI()
 db = DatabaseService()
 response = ResponseFactory()
+manager = ConnectionManager()
 
+# todo: later should be replaced
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # can alter with time
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# время через которое будет удаляться чат; время жизни чата и юзер тоже должен удаляться
 
 @app.post("/sign-up", response_model=schemas.SignUpResponseModel)
 async def sign_up(user: schemas.UserModel, session: scoped_session = Depends(db.get_db)):
     """Signs Up New Users"""
+    # todo: check if user is already signed in
     db.save_user(session, user)
     if db.fetch_user_by_name(session, user.name):
         return response.generate_sign_up_response(True, user)
@@ -39,25 +56,36 @@ async def create_chat(user: schemas.UserModel, session: scoped_session = Depends
     return response.generate_chat_response(False, chat)
 
 
-# TODO: finish other api's
-@app.websocket("/send_message")
-async def send_message(websocket: WebSocket):
-    """Sends Message to Given Chatroom"""
-    connection = ConnectionHandler(websocket)
-    request = connection.get_request()
-    message = {}  # Create Message Object
-    # Save Message Object to DB
-    response = {}  # uses ResponseFactory to generate Response JSON from Message Object
-    connection.send_response(response)
+@app.post('/connect-to-chat', response_model=Union[schemas.UserUndefinedModel,
+                                                   schemas.ChatResponseModel])
+async def connect_to_chat(chat: schemas.ChatRoomModel, session: scoped_session = Depends(db.get_db)):
+    """Connects user to chatroom"""
+    user = chat.users[0]
+    if not (user_db := db.fetch_user_by_name(session, user.name)):
+        return response.generate_user_undefined_error_response(user)
+    if not db.fetch_chat_by_name(session, chat.name):
+        return response.generate_chat_response(status=False, chatroom=chat)
+    db.add_user_to_chatroom(session, chatroom_model=chat, user_model=user)
+
+    if user_db in db.fetch_chat_by_name(session, chat.name).users:
+        return response.generate_chat_response(status=True, chatroom=chat)
+    return response.generate_chat_response(status=False, chatroom=chat)
 
 
-@app.websocket("/get_messages")
-async def get_message(websocket: WebSocket):
-    """Gets All Messages Of Given Chatroom"""
-    connection = ConnectionHandler(websocket)
-    request = connection.get_request()
-    chatroom = (
-        {}
-    )  # Get messsage object from chatroom - something like database_client.get_chatroom_data(chatroom_id)
-    response = {}  # uses ResponseFactory to generate Response JSON from Chatroom Object
-    connection.send_response(response)
+@app.websocket("/ws/{chatroom}/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str, chatroom: str,
+                             session: scoped_session = Depends(db.get_db)):
+    await manager.connect(chatroom, username, websocket)
+    await manager.send_message(chatroom, f'ok')
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    try:
+        while True:
+            text = await websocket.receive_text()
+            message = MessageModel(text=text, user=username)
+            await manager.send_message(chatroom, message.json())
+            print(f'{username} sent "{text}" to {chatroom}')
+
+    except WebSocketDisconnect:
+        manager.disconnect(chatroom, username)
