b3114ec435edf9c8fe34b3c8a90dd054fb3cd44f
Lopkop <84400463+Lopkop@users.noreply.github.com>
2022-09-23 13:07:52 +0300

    rename connection_handler.py to connection_manager.py, refactor ConnectionManager class

--- src/sockets/connection_manager.py
+++ src/sockets/connection_manager.py
@@ -1,18 +1,21 @@
-from typing import List
-
 from fastapi import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: dict[dict] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, chatroom, user_id, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        try:
+            self.active_connections[chatroom][user_id] = websocket
+        except KeyError:
+            self.active_connections[chatroom] = {user_id: websocket}
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, chatroom, user_id):
+        del self.active_connections[chatroom][user_id]
 
-    async def send_message(self, websocket: WebSocket, message):
-        await websocket.send_text(message)
+    async def send_message(self, chatroom, message):
+        print(self.active_connections)
+        for websocket in self.active_connections[chatroom].values():
+            await websocket.send_text(message)
