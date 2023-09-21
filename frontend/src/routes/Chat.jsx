aa2e71cc6e7988099f76a6b9f84dbc3df116e61e
Lopkop <lopkophacked@protonmail.com>
2023-09-21 17:42:34 +0300

    Update frontend: chat page

--- frontend/src/routes/Chat.jsx
+++ frontend/src/routes/Chat.jsx
@@ -3,7 +3,7 @@ import "../styles/App.css";
 import { Footer } from "../components";
 
 async function send_message(event) {
-    // save message, send it to other user,
+    // save message and send it to other user
 }
 
 export default function Chat() {
@@ -12,7 +12,17 @@ export default function Chat() {
             <header>
                 <h1>Chat</h1>
             </header>
-            Work in progress...
+            <main>
+                <div className="chat">
+                    <div className="send-form">
+                        <input id="input-message"
+                            type="text"
+                            placeholder="Write message..."
+                            className="message-button"/>
+                    </div>
+                </div>
+                <button >Send message</button>
+            </main>
             <Footer />
         </div>
     );
