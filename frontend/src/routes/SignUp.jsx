1d89d3fd0c3a318b183551bdcc40cee98125bb09
Mark <lopkophacked@protonmail.com>
2024-02-26 15:50:02 +0300

    add conditional rendering to SignUp component, refactor code

--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { React, useEffect, useState }  from "react";
+
 import "../styles/App.css";
-import { Footer } from "../components";
+import { user_is_logged_in, Footer, get_user } from "../components";
 
 async function signup(event) {
     let username = document.getElementById("username").value;
@@ -26,34 +27,55 @@ async function signup(event) {
     }
 }
 
-export default function SignUp() {
-    return (
-        <div className="App">
-            <header>
-                <h1>Chat Service</h1>
-            </header>
-            <main>
-                <input
-                    id="username"
-                    type="text"
-                    className="connect-chat"
-                    placeholder="Username"
-                />
-                <input
-                    id="password"
-                    type="password"
-                    className="connect-chat"
-                    placeholder="Password"
-                />
-                <input
-                    id="lifetime"
-                    type="integer"
-                    className="connect-chat"
-                    placeholder="Lifetime"
-                />
-                <button onClick={(event) => signup(event)}>Sign Up</button>
-            </main>
-            <Footer />
-        </div>
-    );
+function Signup_render() {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        async function fetchData() {
+            const logged_in = await user_is_logged_in();
+            setUser(logged_in);
+            }
+            fetchData();
+        }, [])
+    if (user) {
+        window.location.pathname = `/chats`;
+    } else {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Chat Service</h1>
+                </header>
+                <main>
+                    <input
+                        id="username"
+                        type="text"
+                        className="connect-chat"
+                        placeholder="Username"
+                    />
+                    <input
+                        id="password"
+                        type="password"
+                        className="connect-chat"
+                        placeholder="Password"
+                    />
+                    <input
+                        id="lifetime"
+                        type="integer"
+                        className="connect-chat"
+                        placeholder="Lifetime"
+                    />
+                    <button onClick={(event) => signup(event)}>Sign Up</button>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+
+export default function Login() {
+     return (
+        <section>
+            {Signup_render()}
+        </section>
+    )
 }
