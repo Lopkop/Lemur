da0d32122d5c43afce1cb1645fa8e8b245633263
Mark <lopkophacked@protonmail.com>
2024-02-26 15:03:41 +0300

    update Root component, add conditional rendering, refactor code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
   {
     path: "/chat/:chatName",
     element: <Chat />,
-  },
+  }
 ]);
 
 const root = ReactDOM.createRoot(
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import { React, useEffect, useState }  from "react";
+
 import "../styles/App.css";
 import { Footer } from "../components";
+import { user_is_logged_in } from "../components";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,24 +13,45 @@ function redirect_to_login() {
        window.location.pathname = `/login`;
 }
 
+function Root_render() {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        async function fetchData() {
+            const logged_in = await user_is_logged_in();
+            setUser(logged_in);
+            }
+            fetchData();
+        }, [])
+    if (user) {
+        window.location.pathname = `/chats`;
+    } else {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Chat Service</h1>
+                </header>
+                <main>
+                    <span className="some-text">The world is dual, so you have only two choices.</span>
+                    <div className="auth-buttons-container">
+                      <button onClick={redirect_to_login} variant="secondary" className="login-button">
+                        Login
+                      </button>
+                      <button onClick={redirect_to_signup} variant="secondary" className="signup-button">
+                        Sign Up
+                      </button>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+
 export default function Root() {
     return (
-        <div className="App">
-            <header>
-                <h1>Chat Service</h1>
-            </header>
-            <main>
-                <span className="some-text">The world is dual, so you have only two choices.</span>
-                <div className="auth-buttons-container">
-                  <button onClick={redirect_to_login} variant="secondary" className="login-button">
-                    Login
-                  </button>
-                  <button onClick={redirect_to_signup} variant="secondary" className="signup-button">
-                    Sign Up
-                  </button>
-                </div>
-            </main>
-            <Footer />
-        </div>
-    );
+        <section>
+            {Root_render()}
+        </section>
+    )
 }
