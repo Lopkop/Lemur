4d5fd5ed6903be9e3a46a95313abad9882e2f9ab
Mark <lopkophacked@protonmail.com>
2024-03-15 11:04:53 +0300

    refactor database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from sqlalchemy.orm import declarative_base
 
 from config import settings
-
-engine = create_engine(settings.DATABASE_URL)
-
-SessionLocal = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 
 
-def create_tables():
+def _create_tables():
+    engine = create_engine(settings.DATABASE_URL)
     Base.metadata.create_all(bind=engine)
