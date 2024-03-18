8187e4a3b006c9a88779d19cb692ed421704af35
Mark <lopkophacked@protonmail.com>
2024-03-18 10:01:09 +0300

    add exception handler to backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.api_settings import get_api_settings
+from fastapi.responses import JSONResponse
 
 from auth.api import auth_router
 from chatroom.api import chat_router
@@ -31,6 +32,16 @@ router = APIRouter(prefix="/api")
 router.include_router(auth_router, tags=['Auth'])
 router.include_router(chat_router, tags=['Chat'])
 router.include_router(socket_router, tags=['Socket'])
+app.include_router(router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        headers=exc.headers,
+        content={"message": exc.detail})
+
 
 if __name__ == "__main__":
     uvicorn.run(app, log_level=settings.LOG_LEVEL)
