e397ff674b4328e3ff01423ce8304753b94ffb2f
Mark <lopkophacked@protonmail.com>
2024-04-02 14:55:31 +0300

    refactor connectionManager

--- backend/sockets/connection_manager.py
+++ backend/sockets/connection_manager.py
@@ -5,15 +5,15 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[dict] = {}
 
-    async def connect(self, chatroom, user_id, websocket: WebSocket):
+    async def connect(self, chatroom, username, websocket: WebSocket):
         await websocket.accept()
         try:
-            self.active_connections[chatroom][user_id] = websocket
+            self.active_connections[chatroom][username] = websocket
         except KeyError:
-            self.active_connections[chatroom] = {user_id: websocket}
+            self.active_connections[chatroom] = {username: websocket}
 
-    def disconnect(self, chatroom, user_id):
-        del self.active_connections[chatroom][user_id]
+    def disconnect(self, chatroom, username):
+        del self.active_connections[chatroom][username]
 
     async def send_message(self, chatroom, message):
         for websocket in self.active_connections[chatroom].values():
