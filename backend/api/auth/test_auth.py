6a710385834630385d280b18fb6bb038e4523219
Mark <lopkophacked@protonmail.com>
2024-05-14 11:57:13 +0300

    refactor sign_up endpoint test, add log_in endpoint test

--- backend/api/auth/test_auth.py
+++ backend/api/auth/test_auth.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+from fastapi import Depends
 from fastapi.testclient import TestClient
 
 from main import app
@@ -17,4 +18,18 @@ client = TestClient(app)
 def test_sign_up(fill_db, user: dict, status: int):
     req = client.post('/api/sign-up', content=json.dumps(user))
     assert req.status_code == status
-    assert 1 == 1
+
+
+@pytest.mark.parametrize('user,status',
+                         [
+                             ({'name': 'new_user', 'password': 'test', 'lifetime': 1}, 401),
+                             ({'name': 'existing_user', 'password': 'wrong_password', 'lifetime': 2}, 401),
+                             ({'name': 'existing_user', 'password': 'test', 'lifetime': 2}, 200),
+                             ({'name': 'bad_user', 'pass': 'test'}, 422),
+                         ])
+def test_log_in(db_connection, fill_db, user: dict, status: int):
+    token_model = db_connection[1].fetch_token_by_username(next(db_connection[1].get_db())
+                                                     , user.get('name'))
+    client.cookies = {'access_token': token_model and token_model.token}
+    req = client.post('/api/login', content=json.dumps(user))
+    assert req.status_code == status
